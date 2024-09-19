c0122dd62f5be9281594193fb98aff4121536e98
dudatiemiak <rm554756@fiap.com.br>
2024-09-18 22:52:52 -0300

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,12 @@ import { NavLink } from "react-router-dom";
 export default function Header() {
     return (
         <>
-            <header>
+            <header className="flex bg-purple-800 text-white justify-around items-center h-14">
                 <div>
                     <h1>Portal Filmes</h1>
                 </div>
                 <nav>
-                    <ul>
+                    <ul className="flex gap-4">
                         <li><NavLink to="/">Home</NavLink></li>
                         <li><NavLink to="/movies">Filmes</NavLink></li>
                         <li><NavLink to="/genre">Gêneros</NavLink></li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,17 @@
+import CardContainer from "../components/CardContainer";
+import MovieCard from "../components/MovieCard"
+import movies from "../data/movies.json"
+
 export default function Home(){
     return(
-        <h1> Home Page</h1>
+        <>
+            <CardContainer titulo="Filmes Antigos">
+                {
+                    movies.map(filme => (
+                        <MovieCard key={filme.id} {...filme}/>
+                    ))
+                }
+            </CardContainer>
+        </>
     )
 }
